0d587d8bd49982b672a5b47ae0bc723051661247
davidnx01 <info@webdavid.sk>
2023-04-14 12:55:08 +0200

    Hero Banner

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { FaFacebook, FaTiktok } from "react-icons/fa"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <>
+    <div className='max-w-[1720px] w-[100%] mx-auto mt-20'>
+      <h1 className='light_active_color text-7xl'>THE BARBERSHOP</h1>
+      <h2 className='active_color font-bold text-3xl mt-6'>EXPERT HAIRCUTS AND GROOMING SERVICE</h2>
+      <p className='light_active_color text-lg max-w-[620px] w-[100%] mt-6 opacity-70 p_lh font-light'>At The Finest Barbershop, we offer a wide 
+      range of professional hair cutting and grooming 
+      services for men. Our experienced barbers use 
+      the latest techniques and tools to give you the 
+      perfect look. Whether you're in need of a classic 
+      haircut, straight razor shave, or beard trim, 
+      we've got you covered.</p>
+      <div className='flex flex-row gap-6 items-center mt-12'>
+        <button className='active_bg py-6 px-8 light_active_color text-lg font-bold'>BOOK A HAIRCUT</button>
+        <button className='bg-transparent active_border text-lg font-bold py-6 px-8 active_color'>START SHOPPING</button>
+      </div>
+
+      <div className='flex flex-row items-center gap-6 mt-10 light_active_color'>
+        <div className='active_bg h-10 w-10 rounded-full flex items-center justify-center cursor-pointer'>
+          <FaTiktok size={20} />
+        </div>
+
+        <div className='active_bg h-10 w-10 rounded-full flex items-center justify-center cursor-pointer'>
+          <FaFacebook size={20} />
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
--- components/NavigationBar.jsx
+++ components/NavigationBar.jsx
@@ -4,7 +4,7 @@ import { HiOutlineShoppingBag } from 'react-icons/hi2'
 
 const NavigationBar = () => {
   return (
-    <div className='w-full py-8 flex items-center justify-center'>
+    <div className='w-full py-8 px-4 flex items-center justify-center'>
         <div className='max-w-[1720px] w-[100%] flex flex-row items-center justify-between font-bold light_active_color text-sm'>
             <img src="/images/logo.png" alt="Logo" />
                 <ul>
@@ -18,7 +18,7 @@ const NavigationBar = () => {
                 </ul>
             <div className='flex flex-row items-center gap-2'>
                 <div className='flex flex-row items-center gap-4'>
-                    <HiOutlineShoppingBag color='var(--light_active)' className='cursor-pointer' size={20}/>
+                    <HiOutlineShoppingBag color='#ffffff' className='cursor-pointer' size={20}/>
                     <p className='cursor-pointer'>CARD</p>
                 </div>
                     <span className='h-6 w-6 rounded-full active_bg flex items-center justify-center cursor-pointer'>
